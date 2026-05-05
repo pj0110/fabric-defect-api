ba7ef4e37b83c77688a2d7843ebd9be9f8174f72
prashant <jhaprashant2u@gmail.com>
2026-05-05 11:10:35 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, request, jsonify
+import tensorflow as tf
+import numpy as np
+from PIL import Image
+import os
+
+app = Flask(__name__)
+
+# Load model
+model = tf.keras.models.load_model("model.h5")
+
+def preprocess(image):
+    image = image.resize((224,224))
+    image = np.array(image) / 255.0
+    image = np.expand_dims(image, axis=0)
+    return image
+
+@app.route("/")
+def home():
+    return "Fabric Defect Detection API Running"
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    file = request.files["file"]
+    image = Image.open(file).convert("RGB")
+
+    img = preprocess(image)
+    pred = model.predict(img)
+
+    result = "Defective" if pred[0][0] > 0.5 else "Good"
+
+    return jsonify({"prediction": result})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
