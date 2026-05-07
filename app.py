9482508b56c13b58507c8cf05d97b41e4150f64e
prashant <jhaprashant2u@gmail.com>
2026-05-07 12:17:46 +0530

    Added frontend UI

--- app.py
+++ app.py
@@ -1,68 +1,59 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
+from flask_cors import CORS
 import tensorflow as tf
 import numpy as np
 from PIL import Image
-import os
+import io
 
 app = Flask(__name__)
 
-# 🔐 API KEY from Render Environment
-API_KEY = os.environ.get("API_KEY")
+CORS(app)
 
-# Load model once (global)
-model = None
+# Load trained model
+model = tf.keras.models.load_model("fabric_model.h5")
 
-def load_model():
-    global model
-    if model is None:
-        model = tf.keras.models.load_model("model.h5", compile=False)
-    return model
+# Classes
+classes = ["good", "defective"]
 
-def preprocess(image):
+# Image preprocessing
+def preprocess_image(image):
     image = image.resize((224, 224))
     image = np.array(image) / 255.0
     image = np.expand_dims(image, axis=0)
     return image
 
+# Home route
 @app.route("/")
 def home():
-    return "Fabric Defect Detection API Running"
+    return render_template("index.html")
 
+# Prediction route
 @app.route("/predict", methods=["POST"])
 def predict():
-    try:
-        # 🔐 API KEY CHECK
-        key = request.headers.get("x-api-key")
-        if key != API_KEY:
-            return jsonify({"error": "Unauthorized"}), 401
 
-        # Load model
-        model = load_model()
+    if "file" not in request.files:
+        return jsonify({
+            "error": "No file uploaded"
+        })
 
-        # Get image
-        if "file" not in request.files:
-            return jsonify({"error": "No file uploaded"}), 400
+    file = request.files["file"]
 
-        file = request.files["file"]
-        image = Image.open(file).convert("RGB")
+    image = Image.open(
+        io.BytesIO(file.read())
+    ).convert("RGB")
 
-        # Preprocess
-        img = preprocess(image)
+    processed = preprocess_image(image)
 
-        # Prediction
-        pred = model.predict(img)
+    prediction = model.predict(processed)
 
-        print("RAW PRED:", pred)
+    predicted_class = classes[np.argmax(prediction)]
 
-        # Output
-        result = "Defective" if pred[0][0] > 0.5 else "Good"
+    confidence = float(np.max(prediction))
 
-        return jsonify({"prediction": result})
-
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
-
+    return jsonify({
+        "prediction": predicted_class,
+        "confidence": round(confidence * 100, 2)
+    })
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(debug=True)
